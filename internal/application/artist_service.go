a01c29adfb30c143bcaea8a800150b6871cc200f
Your Name <you@example.com>
2025-09-03 21:21:51 +0500

    first steps

--- internal/application/artist_service.go
+++ internal/application/artist_service.go
@@ -1,1 +1,12 @@
 package application
+
+import (
+	"groupie-tracker/internal/"
+)
+
+type ArtistService struct {}
+
+
+func (s *ArtistService) GetAllArtist() ([]domian.Artist, error) {
+	return api.FetchArtist()
+}
